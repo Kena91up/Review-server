02c1169699897bed05f2f735171eb7c10d28283d
Viktoria <viktoria.mihaylova92@gmail.com>
2021-03-04 21:08:54 +0100

    Routes for reviews created

--- models/Review.model.js
+++ models/Review.model.js
@@ -1,18 +1,19 @@
 const { Schema, model } = require("mongoose");
 
-const reviewSchema = new Schema ({
+const reviewSchema = new Schema({
   userId: {
     type: Schema.Types.ObjectId,
-    ref: 'User'
-    },
-    title: String,
-    image: String,
-    description: String,
-    date:{
-       type: Date,
-       default: Date.now
-    }
-})
+    ref: "User",
+  },
+  title: String,
+  image: String,
+  description: String,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  rating: String,
+});
 
 const Review = model("Review", reviewSchema);
 
