d816585a5bc3fed69ee1638644724da710c6fc68
kena1991 <trivedikena@gmail.com>
2021-03-10 23:40:38 +0100

    deploy

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -171,7 +171,7 @@ router.patch("/user", isLoggedIn, (req, res, next) => {
     });
 });
 //delete for userprofile
-router.delete("/user/delete", (req, res) => {
+router.delete("/user/:id", (req, res) => {
   User.findByIdAndDelete(req.params.id)
     .then((response) => {
       res.status(200).json(response);
